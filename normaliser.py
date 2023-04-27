d83164d1dbe078d88e7d3c5c5b5fcb80b0b90847
Dan Ruttley <81565663+danruttley@users.noreply.github.com>
2023-04-27 10:53:00 +0100

    :sparkles: Add normalisation based on integrated trap power

--- normaliser.py
+++ normaliser.py
@@ -51,70 +51,60 @@ def load_holos_from_params(filename):
 if __name__ == '__main__':
     slm = SLM(monitor=1)
     cam = Camera(exposure=0.1,gain=0,roi=[1179,197,1297,228])
+    center = (252,272)
+    waist = 215
+    imager = ImageHandler(measure_folder=r"Z:\Tweezer\Experimental Results",
+                        bit_depth=cam.get_bit_depth())
 
-    measures = []
-    radius_scales = np.linspace(0.5,1,26)
-    np.random.shuffle(radius_scales)
-    for radius_scale in radius_scales:
-        center = (252,272)
-        waist = 215
-        # radius_scale = 0.8 # semi minor to major ratio for ellipse
+    correction_factor = 0.2
+    radius_scale = 0.8
 
-        imager = ImageHandler(measure_folder=r"Z:\Tweezer\Experimental Results",
-                            bit_depth=cam.get_bit_depth(),measure_params={'radius_scale':radius_scale})
-        
-        correction_factor = 0.2
+    exposure = 1
+    gain = 0
+    exposure = cam.update_exposure(exposure)
+    gain = cam.update_gain(gain)
 
-        exposure = 1
-        gain = 0
-        exposure = cam.update_exposure(exposure)
-        gain = cam.update_gain(gain)
+    # holo = (hg.gratings.hori(period=7)+
+    #         hg.gratings.hori_gradient(gradient=-10.2)+
+    #         hg.lenses.focal_plane_shift(shift=-15.68,x0=265,y0=251)+
+    #         hg.gratings.vert_gradient(gradient=9.6)+
+    #         hg.zernike(4,0,-0.129,265,251,247)+
+    #         hg.zernike(2,2,-0.2,265,251,247)+
+    #         hg.zernike(2,-2,-0.076,265,251,247)+
+    #         hg.zernike(4,4,0.08,265,251,247))%1
+    # holo = hg.apertures.circ(holo,x0=265,y0=251,radius=247)
 
-        # holo = (hg.gratings.hori(period=7)+
-        #         hg.gratings.hori_gradient(gradient=-10.2)+
-        #         hg.lenses.focal_plane_shift(shift=-15.68,x0=265,y0=251)+
-        #         hg.gratings.vert_gradient(gradient=9.6)+
-        #         hg.zernike(4,0,-0.129,265,251,247)+
-        #         hg.zernike(2,2,-0.2,265,251,247)+
-        #         hg.zernike(2,-2,-0.076,265,251,247)+
-        #         hg.zernike(4,4,0.08,265,251,247))%1
-        # holo = hg.apertures.circ(holo,x0=265,y0=251,radius=247)
+    holo = load_holos_from_params(r"Z:\Tweezer\Experimental Results\2023\April\26\normaliser_base_no_ZK.txt")
 
-        holo = load_holos_from_params(r"Z:\Tweezer\Experimental Results\2023\April\26\normaliser_base_no_ZK.txt")
+    traps = [(284, 256, 1.04574516), (280, 256, 1.11698361), (276, 256, 0.87086792), (272, 256, 1.02632144), (268, 256, 0.93701741), (264, 256, 0.87218266), (260, 256, 0.86132075), (256, 256, 1.22706198), (252, 256, 1.04249907)]
+    padding = 1
 
-        traps = [(368, 256, 1.166851), (352, 256, 1.113433), (336, 256, 1.020398), (320, 256, 0.940435), (304, 256, 0.810454), (288, 256, 0.997138), (272, 256, 1.011309), (256, 256, 1.0375530000000002), (240, 256, 1.0392629999999998)]
-        padding = 4
+    array_gen = ArrayGenerator(slm,cam,imager,holo,
+                            circ_aper_center=center,circ_aper_radius=247,
+                            input_waist=waist,input_center=center,
+                            correction_factor=correction_factor,
+                            min_distance_between_traps=5,
+                            remove_low_zernike=True,padding=padding)
+    array_gen.traps = traps
+    array_gen.get_single_trap_coords()
+    # array_gen.load_trap_df(r"Z:\Tweezer\Experimental Results\2023\April\26\SLM measures\Measure 10\trap_df.csv") # warning: this will overwrite target_Is
+    array_gen.traps = traps
 
-        array_gen = ArrayGenerator(slm,cam,imager,holo,
-                                circ_aper_center=center,circ_aper_radius=247,
-                                input_waist=waist,input_center=center,
-                                correction_factor=correction_factor,
-                                min_distance_between_traps=5,
-                                remove_low_zernike=True,padding=padding)
-        array_gen.traps = traps
-        # array_gen.get_single_trap_coords()
-        array_gen.load_trap_df(r"Z:\Tweezer\Experimental Results\2023\April\26\SLM measures\Measure 10\trap_df.csv")
-        array_gen.traps = traps
+    exposure = cam.update_exposure(exposure)
+    gain = cam.update_gain(gain)
 
-        exposure = cam.update_exposure(exposure)
-        gain = cam.update_gain(gain)
-
-        array_gen.generate_initial_hologram(iterations=50)
+    array_gen.generate_initial_hologram(iterations=50)
+    array_gen.array_holo = hg.apertures.ellipse(array_gen.array_holo,x0=center[0],y0=center[1],radius=240,radius_scale=radius_scale)
+    array_gen.get_trap_depths(reps=1,plot=True)
+    array_gen.save_trap_df()
+    for i in range(50):
+        array_gen.generate_corrected_hologram(iterations=50)
         array_gen.array_holo = hg.apertures.ellipse(array_gen.array_holo,x0=center[0],y0=center[1],radius=240,radius_scale=radius_scale)
         array_gen.get_trap_depths(reps=1,plot=True)
-        array_gen.save_trap_df()
-        # for i in range(50):
-        #     array_gen.generate_corrected_hologram(iterations=50)
-        #     array_gen.array_holo = hg.apertures.ellipse(array_gen.array_holo,x0=center[0],y0=center[1],radius=240,radius_scale=radius_scale)
-        #     array_gen.get_trap_depths(reps=1,plot=True)
-        #     print(array_gen.traps)
-        array_gen.save_trap_df()
-        slm.apply_hologram(array_gen.array_holo)
-
-        print("program completed")
-        print("final trap coordinates:")
         print(array_gen.traps)
+    array_gen.save_trap_df()
+    slm.apply_hologram(array_gen.array_holo)
 
-        measures.append(imager.measure)
-        for measure, radius_scale in zip(measures,radius_scales):
-            print(f'Measure {measure} = radius_scale {radius_scale}')
+    print("program completed")
+    print("final trap coordinates:")
+    print(array_gen.traps)
